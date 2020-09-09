b85b39f6e1d65e43ab676c98ceb07008301dc2f3
ludnikban <ludnikban@gmail.com>
2020-09-09 19:59:36 +0300

    add form into HTML for get size of Canvas

--- LESSON-3/oop/task_3.js
+++ LESSON-3/oop/task_3.js
@@ -5,12 +5,27 @@ class Shape {
     this.lineWidth = options.lineWidth;
     this.selector = document.getElementById(options.selector);
 
+<<<<<<< Updated upstream
     const canvasCreate = document.createElement('canvas');
     canvasCreate.setAttribute('id', options.canvasId);
     canvasCreate.setAttribute('width', '400');
     canvasCreate.setAttribute('height', '400');
     this.selector.appendChild(canvasCreate);
     this.selector = document.getElementById(options.canvasId);
+=======
+  createCanvas(w) {
+    if (document.getElementById(this.canvasId) !== null) {
+      document.getElementById(this.canvasId).setAttribute('width', w);
+      document.getElementById(this.canvasId).setAttribute('height', w);
+    } else {
+      const canvasCreate = document.createElement('canvas');
+      canvasCreate.setAttribute('id', this.canvasId);
+      canvasCreate.setAttribute('width', w);
+      canvasCreate.setAttribute('height', w);
+      this.selector.appendChild(canvasCreate);
+      this.selector = document.getElementById(this.canvasId);
+    }
+>>>>>>> Stashed changes
   }
   draw() {
     this.selector.getContext('2d').fill();
@@ -19,30 +34,58 @@ class Shape {
 }
 
 class Square extends Shape {
+<<<<<<< Updated upstream
   draw() {
     this.selector.getContext('2d').strokeStyle = this.strokeStyle;
     this.selector.getContext('2d').lineWidth = this.lineWidth;
     this.selector.getContext('2d').strokeRect(100, 100, 200, 200);
+=======
+
+  draw(w) {
+    super.createCanvas(w);
+    this.selector.getContext('2d').strokeStyle = this.strokeStyle;
+    this.selector.getContext('2d').lineWidth = this.lineWidth;
+    this.selector.getContext('2d').strokeRect(w / 4, w / 4, w / 2, w / 2);
+    this.selector.getContext('2d').strokeRect(w / 4, w / 4, w / 2, w / 2);
+>>>>>>> Stashed changes
   }
 }
 
 class Circle extends Shape {
+<<<<<<< Updated upstream
   draw() {
+=======
+  draw(w) {
+    super.createCanvas(w);
+>>>>>>> Stashed changes
     this.selector.getContext('2d').beginPath();
     this.selector.getContext('2d').fillStyle = this.fillStyle;
     this.selector.getContext('2d').strokeStyle = this.strokeStyle;
     this.selector.getContext('2d').lineWidth = this.lineWidth;
+<<<<<<< Updated upstream
     this.selector.getContext('2d').arc(200, 200, 100, 0, Math.PI * 2, true);
+=======
+    this.selector.getContext('2d').arc(w / 2, w / 2, w / 4, 0, Math.PI * 2, true);
+>>>>>>> Stashed changes
     super.draw();
   }
 }
 
 class Triange extends Shape {
+<<<<<<< Updated upstream
   draw() {
     this.selector.getContext('2d').beginPath();
     this.selector.getContext('2d').moveTo(200, 100);
     this.selector.getContext('2d').lineTo(100, 300);
     this.selector.getContext('2d').lineTo(300, 300);
+=======
+  draw(w) {
+    super.createCanvas(w);
+    this.selector.getContext('2d').beginPath();
+    this.selector.getContext('2d').moveTo(w / 2, w / 4);
+    this.selector.getContext('2d').lineTo(w / 4, w * 3 / 4);
+    this.selector.getContext('2d').lineTo(w * 3 / 4, w * 3 / 4);
+>>>>>>> Stashed changes
     this.selector.getContext('2d').closePath();
     this.selector.getContext('2d').fillStyle = this.fillStyle;
     this.selector.getContext('2d').strokeStyle = this.strokeStyle;
@@ -75,6 +118,21 @@ const triange = new Triange({
   lineWidth: 5
 })
 
+<<<<<<< Updated upstream
 square.draw();
 circle.draw();
 triange.draw();
+=======
+function CheckWidth(width) {
+  if(width<100)
+    return "100";
+  else
+    return  width;
+}
+
+function Complete() {
+  square.draw(document.SizeCanvas.square.value);
+  circle.draw(document.SizeCanvas.circle.value);
+  triange.draw(document.SizeCanvas.triange.value);
+}
+>>>>>>> Stashed changes
